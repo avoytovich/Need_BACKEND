3d3a4b3d9911ea30628e4899adf32557b39393a9
avoytovich <levanwork@gmail.com>
2023-06-02 00:49:13 +0200

    adjust: deploy

--- server/models/index.js
+++ server/models/index.js
@@ -9,7 +9,7 @@ const db = {};
 
 let sequelize;
 if (process.env.NODE_ENV === 'production') {
-  sequelize = new Sequelize(process.env.DATABASE_URL, {dialect: 'postgres'});
+  sequelize = new Sequelize(process.env.DATABASE_URL);
 } else {
   const config = require(__dirname + '/../../config/config.json')[env];
   sequelize = new Sequelize(config.database, config.username, config.password, config);
